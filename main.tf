ae257a045614ddead361325bf36ac155cda5dda9
dado_hp <odadph@gmail.com>
2020-10-09 17:28:38 +0700

    add files

---
+++ main.tf
@@ -0,0 +1,21 @@
+#Remote Backend Configuration
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "bizzy"
+
+    workspaces {
+      name = "aws_development"
+    }
+  }
+}
+#Provider Configuration
+provider "aws" {
+    region = "us-east-2"
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
+#S3 Bucket Configuration
+resource "aws_s3_bucket" "test" {
+    bucket_prefix = "bsd-s3-from-tf-cloud"
+}
