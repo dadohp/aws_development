d64861f126f935b3e8f24d70810b67f8edd8f820
dado_hp <odadph@gmail.com>
2020-10-09 17:35:46 +0700

    create bucket edit

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -9,12 +9,7 @@ terraform {
     }
   }
 }
-#Provider Configuration
-provider "aws" {
-    region = "us-east-2"
-    access_key = var.access_key
-    secret_key = var.secret_key
-}
+
 #S3 Bucket Configuration
 resource "aws_s3_bucket" "bucket_test" {
     bucket = "bsd-s3-from-tf-cloud"
